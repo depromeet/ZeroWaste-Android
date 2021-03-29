8fc84fcbca2b25260166a59f1de0b0678ed579a0
jackjackE <tmddjs210@naver.com>
2021-03-29 19:45:01 +0900

    BaseActivity.kt , BaseFragment.kt 추가 및 MainActivity.kt 예시

--- app/src/main/java/com/depromeet/zerowaste/feature/main/MainActivity.kt
+++ app/src/main/java/com/depromeet/zerowaste/feature/main/MainActivity.kt
@@ -1,13 +1,12 @@
 package com.depromeet.zerowaste.feature.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.depromeet.zerowaste.R
+import com.depromeet.zerowaste.comm.BaseActivity
+import com.depromeet.zerowaste.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
     }
 }
