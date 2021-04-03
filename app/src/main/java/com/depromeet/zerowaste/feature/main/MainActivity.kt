8270e9f3ce124a908e31af72f34a9d1fac5e6250
jackjackE <tmddjs210@naver.com>
2021-04-03 23:33:14 +0900

    Firebase Analytics 초기화

--- app/src/main/java/com/depromeet/zerowaste/feature/main/MainActivity.kt
+++ app/src/main/java/com/depromeet/zerowaste/feature/main/MainActivity.kt
@@ -7,19 +7,23 @@ import androidx.navigation.fragment.NavHostFragment
 import com.depromeet.zerowaste.R
 import com.depromeet.zerowaste.comm.BaseActivity
 import com.depromeet.zerowaste.databinding.ActivityMainBinding
+import com.google.firebase.analytics.FirebaseAnalytics
+
 
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
 
-    private val viewModel : MainViewModel by viewModels()
+    private val viewModel: MainViewModel by viewModels()
+    private lateinit var mFirebaseAnalytics: FirebaseAnalytics
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this)
         viewModel.test.observe(this) {
             Log.d(null, it)
         }
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.main_navigation_view) as NavHostFragment
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.main_navigation_view) as NavHostFragment
         val navController = navHostFragment.navController
         navController.addOnDestinationChangedListener { controller, destination, arguments ->
             Log.d(null, "id = ${destination.id}, label = ${destination.label}")
