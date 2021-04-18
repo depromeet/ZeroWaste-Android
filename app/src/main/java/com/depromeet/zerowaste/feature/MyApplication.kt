1f39bd6b9110a810b36b0b337a8ce46466c56c39
jackjackE <tmddjs210@naver.com>
2021-04-18 19:33:32 +0900

    Application 클래스 생성 및 카카오로그인 API 키 세팅

---
+++ app/src/main/java/com/depromeet/zerowaste/feature/MyApplication.kt
@@ -0,0 +1,18 @@
+package com.depromeet.zerowaste.feature
+
+import android.app.Application
+import android.util.Log
+import com.depromeet.zerowaste.BuildConfig
+import com.kakao.sdk.common.KakaoSdk
+import com.kakao.sdk.common.util.Utility
+
+class MyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // 다른 초기화 코드들
+
+        // Kakao SDK 초기화
+        KakaoSdk.init(this, BuildConfig.KAKAO_API_KEY)
+        Log.d("HashKey", Utility.getKeyHash(this))
+    }
+}
