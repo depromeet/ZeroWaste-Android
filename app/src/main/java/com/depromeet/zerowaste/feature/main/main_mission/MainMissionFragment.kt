4a59b8b5a3944c68d1e86dc17dcbdbf6e91939f9
Kyun-J <dvkyun@gmail.com>
2021-05-17 18:10:01 +0900

    메인 미션화면 작업중

--- app/src/main/java/com/depromeet/zerowaste/feature/main/main_mission/MainMissionFragment.kt
+++ app/src/main/java/com/depromeet/zerowaste/feature/main/main_mission/MainMissionFragment.kt
@@ -3,7 +3,10 @@ package com.depromeet.zerowaste.feature.main.main_mission
 import androidx.fragment.app.viewModels
 import com.depromeet.zerowaste.R
 import com.depromeet.zerowaste.comm.BaseFragment
+import com.depromeet.zerowaste.comm.SpanStrBuilder
 import com.depromeet.zerowaste.databinding.FragmentMainMissionBinding
+import com.depromeet.zerowaste.feature.main.main_community.MainCommunityListAdapter
+import com.depromeet.zerowaste.feature.main.main_community.MainCommunityViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -14,6 +17,11 @@ class MainMissionFragment :
 
     override fun init() {
         binding.vm = viewModel
-    }
+
+        binding.mainMissionTxt.text = SpanStrBuilder(requireContext())
+            .add("카페", colorRes = R.color.point, sp = 20f)
+            .add(requireContext().resources.getString(R.string.main_mission_suggest)).build()
+
+   }
 
 }
