640bd42f41622506753b6019b2396d273a9f75ea
jackjackE <tmddjs210@naver.com>
2021-05-19 16:52:44 +0900

    [Feat] 로그인 페이지 디자인 구현

---
+++ app/src/main/java/com/depromeet/zerowaste/feature/login/LoginFragment.kt
@@ -0,0 +1,12 @@
+package com.depromeet.zerowaste.feature.login
+
+import com.depromeet.zerowaste.R
+import com.depromeet.zerowaste.comm.BaseFragment
+import com.depromeet.zerowaste.databinding.FragmentLoginBinding
+
+
+class LoginFragment : BaseFragment<FragmentLoginBinding>(R.layout.fragment_login) {
+    override fun init() {
+
+    }
+}
