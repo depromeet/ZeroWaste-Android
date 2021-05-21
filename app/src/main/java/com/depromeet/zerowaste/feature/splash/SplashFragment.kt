0b995f453aa0d9f4346a78105e728fc1f01d4e80
Kyun-J <dvkyun@gmail.com>
2021-05-21 16:14:48 +0900

    리프레시시 유저 정보 가져오는 코드 추가

--- app/src/main/java/com/depromeet/zerowaste/feature/splash/SplashFragment.kt
+++ app/src/main/java/com/depromeet/zerowaste/feature/splash/SplashFragment.kt
@@ -4,8 +4,11 @@ import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.depromeet.zerowaste.R
 import com.depromeet.zerowaste.comm.BaseFragment
+import com.depromeet.zerowaste.comm.data.Constants
 import com.depromeet.zerowaste.comm.data.Share
+import com.depromeet.zerowaste.comm.getPreference
 import com.depromeet.zerowaste.databinding.FragmentSplashBinding
+import com.depromeet.zerowaste.feature.login.LoginFragmentDirections
 import com.depromeet.zerowaste.feature.login.LoginViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -26,9 +29,14 @@ class SplashFragment : BaseFragment<FragmentSplashBinding>(R.layout.fragment_spl
             // 로그인으로 이동
             findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToLoginFragment())
         } else {
-            viewModel.refresh.observe(this) {
-                // 메인으로 이동
-                findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToMainFragment())
+            viewModel.refresh.observe(this) { res ->
+                res.data?.also { user ->
+                    Share.user = user
+                    Share.authToken = user.token
+                    getPreference(requireContext()).edit().putString(Constants.AUTH_TOKEN, user.token).apply()
+                    // 메인으로 이동
+                    findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToMainFragment())
+                } ?: showToast(resources.getString(R.string.server_login_fail))
             }
             viewModel.refreshToken()
         }
