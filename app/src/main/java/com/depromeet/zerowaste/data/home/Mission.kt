73f1c341e16b12dfb52e74a9e3471bca4e747cce
jackjackE <tmddjs210@naver.com>
2021-05-24 16:37:04 +0900

    [Feat] Mission data class 추가

--- app/src/main/java/com/depromeet/zerowaste/data/home/Mission.kt
+++ app/src/main/java/com/depromeet/zerowaste/data/home/Mission.kt
@@ -4,8 +4,22 @@ package com.depromeet.zerowaste.data.home
 import com.google.gson.annotations.SerializedName
 
 data class Mission(
-    @SerializedName("data")
-    val data: Data,
-    @SerializedName("error_code")
-    val errorCode: Int
+    @SerializedName("content")
+    val content: String,
+    @SerializedName("difficulty")
+    val difficulty: String,
+    @SerializedName("icon_img_url")
+    val iconImgUrl: String,
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("logo_img_url")
+    val logoImgUrl: String,
+    @SerializedName("name")
+    val name: String,
+    @SerializedName("owner")
+    val owner: Int,
+    @SerializedName("place")
+    val place: String,
+    @SerializedName("theme")
+    val theme: String
 )
