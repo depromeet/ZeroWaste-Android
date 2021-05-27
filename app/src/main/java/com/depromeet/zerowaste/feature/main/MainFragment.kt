1c36edd1b9f482429ab7eef420922fa38641d6f6
Kyun-J <dvkyun@gmail.com>
2021-05-27 14:03:59 +0900

    바텀네비게이션 최신 리소스 적용

--- app/src/main/java/com/depromeet/zerowaste/feature/main/MainFragment.kt
+++ app/src/main/java/com/depromeet/zerowaste/feature/main/MainFragment.kt
@@ -1,5 +1,8 @@
 package com.depromeet.zerowaste.feature.main
 
+import android.content.res.ColorStateList
+import android.util.Log
+import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
@@ -21,6 +24,8 @@ class MainFragment: BaseFragment<FragmentMainBinding>(R.layout.fragment_main) {
         navController.navigatorProvider.addNavigator(keepStateNavigator)
         navController.setGraph(R.navigation.navigation_graph_home)
         binding.mainBottomNavigation.setupWithNavController(navController)
+        binding.mainBottomNavigation.itemIconTintList = null
+        binding.mainBottomNavigation.itemTextColor = ResourcesCompat.getColorStateList(resources, R.color.selector_main_bottom_text_color, null)
 
         viewModel.navDirection.observe(this) {
             findNavController().navigate(it)
