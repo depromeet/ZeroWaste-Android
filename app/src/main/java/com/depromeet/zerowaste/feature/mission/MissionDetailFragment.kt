627f7964ca6b00ea2aa4a144f34bc0466e580274
Kyun-J <dvkyun@gmail.com>
2021-06-02 23:40:49 +0900

    미션수행 작업

--- app/src/main/java/com/depromeet/zerowaste/feature/mission/MissionDetailFragment.kt
+++ app/src/main/java/com/depromeet/zerowaste/feature/mission/MissionDetailFragment.kt
@@ -1,5 +1,8 @@
 package com.depromeet.zerowaste.feature.mission
 
+import android.graphics.Typeface
+import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.core.content.res.ResourcesCompat
 import com.depromeet.zerowaste.R
 import com.depromeet.zerowaste.comm.BaseFragment
 import com.depromeet.zerowaste.databinding.FragmentMissionDetailBinding
@@ -7,5 +10,23 @@ import com.depromeet.zerowaste.databinding.FragmentMissionDetailBinding
 class MissionDetailFragment: BaseFragment<FragmentMissionDetailBinding>(R.layout.fragment_mission_detail) {
 
     override fun init() {
+        initTitle()
+    }
+
+    private fun initTitle() {
+        val endFont = ResourcesCompat.getFont(requireContext(), R.font.roboto_bold)
+        val startFont = ResourcesCompat.getFont(requireContext(), R.font.kotra_bold)
+        binding.missionDetailMotion.setTransitionListener(object: MotionLayout.TransitionListener {
+            override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {}
+            override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) {
+                if(p3 > 0.5f && binding.missionDetailTitle.typeface != endFont) {
+                    binding.missionDetailTitle.typeface = endFont
+                } else if(p3 >= 0 && p3 < 0.5f && binding.missionDetailTitle.typeface != startFont) {
+                    binding.missionDetailTitle.typeface = startFont
+                }
+            }
+            override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {}
+            override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {}
+        })
     }
 }
