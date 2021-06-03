e02bc15d1e04be194a1970e34b4a890b9dff9fad
Kyun-J <dvkyun@gmail.com>
2021-06-03 18:50:05 +0900

    미션상세 화면작업중

--- app/src/main/java/com/depromeet/zerowaste/feature/mission/MissionDetailFragment.kt
+++ app/src/main/java/com/depromeet/zerowaste/feature/mission/MissionDetailFragment.kt
@@ -1,8 +1,9 @@
 package com.depromeet.zerowaste.feature.mission
 
-import android.graphics.Typeface
+import android.util.Log
 import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.core.content.res.ResourcesCompat
+import androidx.core.widget.NestedScrollView
 import com.depromeet.zerowaste.R
 import com.depromeet.zerowaste.comm.BaseFragment
 import com.depromeet.zerowaste.databinding.FragmentMissionDetailBinding
@@ -11,6 +12,7 @@ class MissionDetailFragment: BaseFragment<FragmentMissionDetailBinding>(R.layout
 
     override fun init() {
         initTitle()
+        initContent()
     }
 
     private fun initTitle() {
@@ -28,5 +30,15 @@ class MissionDetailFragment: BaseFragment<FragmentMissionDetailBinding>(R.layout
             override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {}
             override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {}
         })
+    }
+
+    private fun initContent() {
+        binding.missionDetailNested.setOnScrollChangeListener(NestedScrollView.OnScrollChangeListener
+        { _, _, scrollY, _, oldScrollY ->
+            if (scrollY > oldScrollY && binding.missionDetailMotion.progress != 1f) {
+                binding.missionDetailMotion.progress += Float.MIN_VALUE
+            }
+        })
+        binding.missionDetailHowToImg.clipToOutline = true
     }
 }
