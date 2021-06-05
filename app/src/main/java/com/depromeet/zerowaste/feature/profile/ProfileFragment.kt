4e3bec7963766d404b1adeb40ab1591a87a52060
jackjackE <tmddjs210@naver.com>
2021-06-05 16:28:11 +0900

    [Refactor] 프로필 - 디자인 수정

--- app/src/main/java/com/depromeet/zerowaste/feature/profile/ProfileFragment.kt
+++ app/src/main/java/com/depromeet/zerowaste/feature/profile/ProfileFragment.kt
@@ -5,12 +5,31 @@ import androidx.navigation.fragment.findNavController
 import com.depromeet.zerowaste.R
 import com.depromeet.zerowaste.comm.BaseFragment
 import com.depromeet.zerowaste.databinding.FragmentProfileBinding
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayout.OnTabSelectedListener
 
-class ProfileFragment: BaseFragment<FragmentProfileBinding>(R.layout.fragment_profile) {
+
+class ProfileFragment : BaseFragment<FragmentProfileBinding>(R.layout.fragment_profile) {
 
     override fun init() {
         requireActivity().onBackPressedDispatcher.addCallback(this) {
             findNavController().popBackStack()
         }
+        initTabLayout()
+    }
+
+    private fun initTabLayout() {
+        binding.mainProfileLayoutTab.addOnTabSelectedListener(object : OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                val position = tab.position
+                if (position == 0) {
+                } else if (position == 1) {
+
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab) {}
+            override fun onTabReselected(tab: TabLayout.Tab) {}
+        })
     }
 }
