330075dc4951b080d8dd1c5d93f1226540da72ad
jackjackE <tmddjs210@naver.com>
2021-06-06 00:46:08 +0900

    [Feat] 프로필 - 비즈니스 로직 구현

--- app/src/main/java/com/depromeet/zerowaste/feature/profile/ProfileFragment.kt
+++ app/src/main/java/com/depromeet/zerowaste/feature/profile/ProfileFragment.kt
@@ -1,21 +1,48 @@
 package com.depromeet.zerowaste.feature.profile
 
 import androidx.activity.addCallback
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.depromeet.zerowaste.R
 import com.depromeet.zerowaste.comm.BaseFragment
+import com.depromeet.zerowaste.comm.BaseRecycleAdapter
+import com.depromeet.zerowaste.data.mission.Mission
 import com.depromeet.zerowaste.databinding.FragmentProfileBinding
+import com.depromeet.zerowaste.databinding.ItemProfileMissionBinding
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayout.OnTabSelectedListener
 
 
 class ProfileFragment : BaseFragment<FragmentProfileBinding>(R.layout.fragment_profile) {
 
+    private val viewModel: ProfileViewModel by viewModels()
+
+    private val missionAdapter =
+        BaseRecycleAdapter(R.layout.item_profile_mission) { item: Mission, bind: ItemProfileMissionBinding, position: Int ->
+            bind.item = item
+            bind.root.setOnClickListener {
+
+            }
+        }
+
     override fun init() {
+        binding.vm = viewModel
         requireActivity().onBackPressedDispatcher.addCallback(this) {
             findNavController().popBackStack()
         }
         initTabLayout()
+        initMissionList()
+    }
+
+    private fun initMissionList() {
+        viewModel.participatedMissionList.observe(this, { data ->
+            missionAdapter.addData(data)
+        })
+        viewModel.likedMissionList.observe(this, { data ->
+            missionAdapter.addData(data)
+        })
+        binding.mainProfileRvMissions.adapter = missionAdapter
+        viewModel.getParticipatedMissionList()
     }
 
     private fun initTabLayout() {
@@ -23,8 +50,9 @@ class ProfileFragment : BaseFragment<FragmentProfileBinding>(R.layout.fragment_p
             override fun onTabSelected(tab: TabLayout.Tab) {
                 val position = tab.position
                 if (position == 0) {
+                    viewModel.getParticipatedMissionList()
                 } else if (position == 1) {
-
+                    viewModel.getLikedMissionList()
                 }
             }
 
